e76560da745e08fe7fa438e6b6b6b39c6c280d9a
Akbar Maulana <amaulanaa995@gmail.com>
2021-01-25 19:16:32 +0700

    created user routes and user controller, modified product routes and product controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -22,6 +23,7 @@ app.get('/', (req,res) => {
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/USERs', userRoutes)
 
 app.use(notFound)
 
